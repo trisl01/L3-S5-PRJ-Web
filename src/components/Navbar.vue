97070f5a28212fb0fa7066699bc7356e9334cd4c
Tristan Loye <73702803+trisl01@users.noreply.github.com>
2022-12-13 03:35:26 -0500

    update add logo and name
    Co-Authored-By: Arthur <51738831+picthur@users.noreply.github.com>
    Co-Authored-By: lucmrn <94229012+lucmrn@users.noreply.github.com>

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,6 +8,10 @@
   <nav class="bg-white">
     <div class="container">
       <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+          <img id="img-logo" src="../assets/logo.png" alt="" srcset="">
+          <span id="text-logo" class="fs-4 font-lobster-two"><i class="italic">Pomme</i></span>
+        </a>
       </header>
     </div>
   </nav>
