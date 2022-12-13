f88ddc06e2f645fa53431dcd395c0378ce1933d7
Tristan Loye <73702803+trisl01@users.noreply.github.com>
2022-12-13 03:40:05 -0500

    update add `AppleCardVue` for every Apple on the json
    Co-Authored-By: Arthur <51738831+picthur@users.noreply.github.com>
    Co-Authored-By: lucmrn <94229012+lucmrn@users.noreply.github.com>

--- src/components/GridApples.vue
+++ src/components/GridApples.vue
@@ -6,6 +6,8 @@
 <template>
   <div class="container">
     <div class="d-flex flex-wrap">
+      <AppleCardVue 
+        v-for="apple in AppleData" :key="apple.id" :name="apple.name" :price="apple.price" :stock="apple.stock" :description="apple.description" :bestMonths="apple['best-months']" :image="apple.image"
     </div>
   </div>
 </template>
