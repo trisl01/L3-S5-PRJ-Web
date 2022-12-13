26f903b6396e7cc73b3ed9a91380862cf26e77dc
Tristan Loye <73702803+trisl01@users.noreply.github.com>
2022-12-13 03:48:26 -0500

    update add list of pages
    Co-Authored-By: Arthur <51738831+picthur@users.noreply.github.com>
    Co-Authored-By: lucmrn <94229012+lucmrn@users.noreply.github.com>

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,6 +4,11 @@
 <template>
   <div class="container">
     <footer class="py-3 my-4">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">Home</a></li>
+        <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">Shop</a></li>
+        <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">Cart</a></li>
+      </ul>
     </footer>
   </div>
 </template>
