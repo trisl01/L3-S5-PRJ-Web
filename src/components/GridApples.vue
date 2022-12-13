0d00bd19b75e1c23c4e3a1792173df5d71e01b38
Tristan Loye <73702803+trisl01@users.noreply.github.com>
2022-12-13 09:49:53 -0500

    update add `<p>` with context

--- src/components/GridApples.vue
+++ src/components/GridApples.vue
@@ -7,6 +7,12 @@
   <div class="container">
     <h1 class="font-lobster-two">Apple Store</h1>
     <br>
+    <p class="font-lobster-two">
+      Welcome to the Apple Store! Here you can find all the apples you need to make your favorite apple pie!<br>
+      We have a wide variety of apples, from the most common ones to the rarest ones. You can find them all here!<br>
+      We have {{ AppleData.length }} different apples in our store!<br>
+      Here are some of our best sellers:
+    </p>
     <div class="d-flex flex-wrap">
       <AppleCardVue 
         v-for="apple in AppleData" :key="apple.id" :name="apple.name" :price="apple.price" :stock="apple.stock" :description="apple.description" :bestMonths="apple['best-months']" :image="apple.image"
