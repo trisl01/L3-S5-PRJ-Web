a5eb61b99ce8828741b4573db211bb70c6d4db81
Tristan Loye <73702803+trisl01@users.noreply.github.com>
2022-12-13 03:35:03 -0500

    update add flex `header`
    Co-Authored-By: Arthur <51738831+picthur@users.noreply.github.com>
    Co-Authored-By: lucmrn <94229012+lucmrn@users.noreply.github.com>

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,6 +7,8 @@
 <template>
   <nav class="bg-white">
     <div class="container">
+      <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      </header>
     </div>
   </nav>
 </template>
